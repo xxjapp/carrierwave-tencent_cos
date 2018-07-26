92a10b4fbbabc5487dec77c82fe8b90b8415cd80
Xia Xionjun <xxjapp@gmail.com>
2018-07-27 01:27:05 +0900

    implement

--- lib/carrierwave/storage/tencent_cos.rb
+++ lib/carrierwave/storage/tencent_cos.rb
@@ -1,43 +1,43 @@
 require 'carrierwave'
+require "tencent_cos_sdk"
 
 module CarrierWave
     module Storage
         class TencentCos < Abstract
             # Create and save a file instance to your engine.
             def store!(file)
-                # File.create(file)
+                f = CarrierWave::Storage::TencentCos::File.new(uploader.store_path)
+                f.store(file)
+                f
             end
 
             # Load and return a file instance from your engine.
             def retrieve!(identifier)
-                # File.find(file)
+                CarrierWave::Storage::TencentCos::File.new(uploader.store_path(identifier))
             end
 
-            # Subclass or duck-type CarrierWave::SanitizedFile ; responsible for storing the file to your engine.
+            # Subclass or duck-type CarrierWave::SanitizedFile
+            # responsible for storing the file to your engine.
             class File
+                attr_reader :path
+
                 # Initialize as required.
-                def initialize()
+                def initialize(path)
+                    @path = path
                 end
 
-                # Duck-type methods for CarrierWave::SanitizedFile.
-                def content_type
+                def store(new_file)
+                    TencentCosSdk.put @path, file: new_file.path
                 end
 
                 def url
-                end
-
-                def read
-                end
-
-                def size
+                    TencentCosSdk.url @path
                 end
 
                 def delete
-                end
-
-                def exists?
+                    TencentCosSdk.delete @path
                 end
             end # File
         end # TencentCos
-    end
-end
+    end # Storage
+end # CarrierWave
--- lib/carrierwave/tencent_cos.rb
+++ lib/carrierwave/tencent_cos.rb
@@ -1,3 +1,4 @@
+require "tencent_cos_sdk"
 require "carrierwave/tencent_cos/version"
 require "carrierwave/storage/tencent_cos"
 
@@ -5,6 +6,19 @@ module CarrierWave
   module TencentCos
     CarrierWave.configure do |conf|
       conf.storage_engines.merge! tencent_cos: 'CarrierWave::Storage::TencentCos'
+    end
+  end
+
+  module Uploader
+    class Base
+      def self.tencent_cos= options
+        TencentCosSdk.configure do |conf|
+          conf.secret_id    = options[:secret_id]
+          conf.secret_key   = options[:secret_key]
+          conf.host         = options[:host]
+          conf.parent_path  = options[:parent_path]
+        end
+      end
     end
   end
 end
